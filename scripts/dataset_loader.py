7139835a2524f8e7cdc92afc3d5b3e4571c8da6c
dominikmuhle <dominikmuhle@icloud.com>
2022-04-17 20:18:35 +0200

    typo

--- scripts/dataset_loader.py
+++ scripts/dataset_loader.py
@@ -2,5 +2,5 @@
 # BSD 3-Clause License
 #
 #
-# Copyright (c) 2022, Sebasitan Muhle, Dominik Muhle.
+# Copyright (c) 2022, Sebastian Muhle, Dominik Muhle.
 # All rights reserved.
--- scripts/helper.py
+++ scripts/helper.py
@@ -2,7 +2,7 @@
 # BSD 3-Clause License
 #
 #
-# Copyright (c) 2022, Sebasitan Muhle, Dominik Muhle.
+# Copyright (c) 2022, Sebastian Muhle, Dominik Muhle.
 # All rights reserved.
 
 import scipy.io
