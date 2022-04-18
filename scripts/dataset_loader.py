9d6e0e81eda3743c78c9adb967c7dd0eca3f6598
Sebastian Muhle <SebastianMuhle@me.com>
2022-04-18 11:13:15 +0200

    Init Transformer

--- scripts/dataset_loader.py
+++ scripts/dataset_loader.py
@@ -9,6 +9,7 @@ import torch
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from skimage import io, transform
+import numpy as np
 
 
 class Oxford102Dataset(Dataset):
@@ -40,4 +41,76 @@ class Oxford102Dataset(Dataset):
             sample = self.transform(sample)
 
         return sample
+
+class Rescale(object):
+    """
+    Rescales the image in a sample to a given size
+    """
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, plant_label = sample["image"], sample["plant_label"]
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+        img = transform.resize(image, (new_h, new_w))
+
+        return {'image': img, 'plant_label': plant_label}
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, plant_label = sample['image'], sample['plant_label']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        plant_label = plant_label - [left, top]
+
+        return {'image': image, 'plant_label': plant_label}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, plant_label = sample['image'], sample['plant_label']
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C x H x W
+        image = image.transpose((2, 0, 1))
+        return {'image': torch.from_numpy(image),
+                'landmarks': torch.from_numpy(plant_label)}
+
 
