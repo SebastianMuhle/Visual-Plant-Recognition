b9e62e9f3e3e97a6b68e62688b81d4c49bb22809
Sebastian Muhle <SebastianMuhle@me.com>
2022-04-18 10:11:20 +0200

    csv conversion finished

--- scripts/helper.py
+++ scripts/helper.py
@@ -12,9 +12,11 @@ from pathlib import Path
 # https: // gist.github.com/techedlaksh/9001039bf54ba9d8aec3ad7f5d8bfd08
 
 
-def convert_mat_to_csv(filename):
-    path = Path(filename)
-    mat = scipy.io.loadmat(path)
+def convert_mat_to_csv(filename: Path):
+    if not filename.exists():
+        print("Could not find file. Please provide the correct path to the file.")
+        return
+    mat = scipy.io.loadmat(filename)
     mat = {k: v for k, v in mat.items() if k[0] != '_'}
     data = pd.DataFrame({k: pd.Series(v[0]) for k, v in mat.items()})
-    data.to_csv(path.with_suffix('.csv'))
+    data.to_csv(filename.with_suffix('.csv'))
