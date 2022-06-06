16ac14c3cc5727282f3b08ec19bf8fded1a908ee
dominikmuhle <dominikmuhle@icloud.com>
2022-06-06 13:14:11 +0200

    removed progress bar

--- scripts/training.py
+++ scripts/training.py
@@ -5,11 +5,12 @@
 # Copyright (c) 2022, Sebastian Muhle, Dominik Muhle.
 # All rights reserved.
 
-def train_epoch(model, dataloader, optimizer, loss_function, use_cuda, progress_bar):
+
+def train_epoch(model, dataloader, optimizer, loss_function, use_cuda):
     model.train()
     for batch_idx, data in enumerate(dataloader):
-        images = data['image']
-        labels = data['plant_label']
+        images = data["image"]
+        labels = data["plant_label"]
         # move to GPU
         if use_cuda:
             data, target = data.cuda(), target.cuda()
@@ -18,7 +19,3 @@ def train_epoch(model, dataloader, optimizer, loss_function, use_cuda, progress_
         loss = loss_function(output, labels)
         loss.backward()
         optimizer.step()
-
-        progress_bar['batch'](batch_idx)
-
-        progress_bar['progress'].update(step=1)
