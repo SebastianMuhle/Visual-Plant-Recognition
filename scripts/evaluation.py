d390dc883a4aa6208074767b0189a650cd199246
dominikmuhle <dominikmuhle@icloud.com>
2022-06-11 16:01:41 +0200

    moved to visualisation

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -5,15 +5,10 @@
 # Copyright (c) 2022, Sebastian Muhle, Dominik Muhle.
 # All rights reserved.
 
-from typing import Tuple
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import torch
 import numpy as np
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.axes import Axes
 
 
 def evaluation(
@@ -42,33 +37,3 @@ def evaluation(
     accuracy = 100.0 * correct / (len(dataloader) * dataloader.batch_size)
     loss /= len(dataloader) * dataloader.batch_size
     return loss, accuracy, conf_matrix
-
-
-def per_class_accuracy(conf_matrix: np.ndarray) -> Figure:
-    # TODO: remove +1 when evaluating the real dataset
-    num_correct = np.diag(conf_matrix) + 1
-    num_total = np.sum(conf_matrix, axis=1) + 1
-
-    accuracy = num_correct / num_total
-
-    fig, ax = plt.subplots(1, 1)
-    ax.hist(accuracy, 20, (0.0, 1.0), density=True)
-    # plt.show()
-    return fig
-
-
-def worst_classes(conf_matrix: np.ndarray, k: int) -> Figure:
-    # TODO: remove +1 when evaluating the real dataset
-    num_correct = np.diag(conf_matrix) + 1
-    num_total = np.sum(conf_matrix, axis=1) + 1
-
-    accuracy = num_correct / num_total
-    worst_k_idx = np.argsort(accuracy)[:k]
-
-    worst_k = conf_matrix[worst_k_idx, :]
-
-    fig, axs = plt.subplots(k, 1)
-    for predictions, ax in zip(worst_k, axs):
-        ax.bar(np.arange(predictions.size) + 1, predictions, align="center")
-    # plt.show()
-    return fig
